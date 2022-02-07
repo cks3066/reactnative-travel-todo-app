ea6f1701ed6a032b222da1821ee8d72570604e44
cks3066 <cks3066@naver.com>
2022-02-08 00:17:23 +0900

    feat: To Dos

--- App.tsx
+++ App.tsx
@@ -12,10 +12,21 @@ import { theme } from "./colors";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => {
     setText(payload);
+  };
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
   };
   return (
     <View style={styles.container}>
@@ -43,6 +54,8 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
+        returnKeyType="done"
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
         value={text}
         placeholder={working ? "Add a To Do" : "Where do you want to do"}
